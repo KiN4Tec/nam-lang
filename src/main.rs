1ddf3a4fd4aca7dc3c2b551f3a63e26706c4a5bd
Mouhammed Nashat Helmi <KiN4Tec@Gmail.com>
2023-07-29 13:45:40 +0300

    Add struct Repl

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,79 @@ mod lexer;
 use color_eyre::eyre::Result;
 use std::io::Write;
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
+struct Repl<'a> {
+    on_init: &'a dyn Fn() -> Result<()>,
+    on_update: &'a dyn Fn(&mut Self, String) -> Result<()>,
+    _is_running: bool,
+}
 
-    // init()
-    {
-        println!("\nConVector v0.0");
+impl<'a> Repl<'a> {
+    pub fn new(
+        on_init: &'a impl Fn() -> Result<()>,
+        on_update: &'a impl Fn(&mut Self, String) -> Result<()>,
+    ) -> Self {
+        Repl {
+            on_init,
+            on_update,
+            _is_running: false,
+        }
     }
 
-    loop {
+    pub fn run(&mut self) -> Result<()> {
+        self._on_init()?;
+
+        while self._is_running {
+            self._on_update()?;
+        }
+
+        Ok(())
+    }
+
+    fn _on_init(&mut self) -> Result<()> {
+        (self.on_init)()?;
+        self._is_running = true;
+        Ok(())
+    }
+
+    fn _on_update(&mut self) -> Result<()> {
         print!("> ");
         std::io::stdout().flush()?;
         let mut input = String::new();
         std::io::stdin().read_line(&mut input)?;
 
+        // _on_exit ()
         if input.trim() == "exit" {
             println!("Goodbye!");
-            break;
+            self._is_running = false;
+            return Ok(());
         }
 
-        let tokens = match lexer::try_parse_from(input) {
-            Ok(tokens) => tokens,
-            Err(e) => {
-                eprintln!("{e:?}");
-                continue;
-            }
-        };
-        dbg!(tokens);
+        (self.on_update)(self, input)?;
+        Ok(())
     }
+}
+
+fn on_init() -> Result<()> {
+    println!("\nConVector v0.0");
+    Ok(())
+}
+
+fn on_update(_repl: &mut Repl, input: String) -> Result<()> {
+    match lexer::try_parse_from(input) {
+        Ok(tokens) => dbg!(tokens),
+        Err(e) => {
+            eprintln!("{e:?}");
+            vec![]
+        },
+    };
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let mut my_repl = Repl::new(&on_init, &on_update);
+    my_repl.run()?;
 
     Ok(())
 }
