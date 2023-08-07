1986921d1c6817fc091cb230ebbc05a541e2146a
Mouhammed Nashat Helmi <KiN4Tec@Gmail.com>
2023-08-07 19:02:38 +0300

    Speed up character iteration in try_tokenize

--- src/lexer.rs
+++ src/lexer.rs
@@ -17,12 +17,13 @@ pub enum Token {
 }
 
 impl Token {
-    fn try_from_str(input: &str) -> Result<Self> {
-        match input
-            .chars()
-            .next()
-            .unwrap_or_else(|| unreachable!("Unexpected empty token"))
-        {
+    fn try_from_str(input: String) -> Result<Self> {
+        let first = match input.chars().next() {
+            Some(c) => c,
+            None => return Err(Report::msg("Unexpected empty token")),
+        };
+
+        match first {
             '+' => Ok(Self::OpAdd),
             '-' => Ok(Self::OpSupstract),
             '*' => Ok(Self::OpMultiply),
@@ -36,74 +37,79 @@ impl Token {
             'A'..='Z' | 'a'..='z' | '_' => {
                 for c in input.chars() {
                     if !(c.is_ascii_alphanumeric() || c == '_') {
-                        return Err(Report::msg(format!("Could not parse charater '{c}'")));
+                        return Err(Report::msg(format!(
+                            "Unexpected character {c} in token {input}"
+                        )));
                     }
                 }
-                Ok(Self::Identifier(input.to_string()))
+                Ok(Self::Identifier(input))
             },
 
-            first => Err(Report::msg(format!("Unexpected character {first}"))),
+            first => Err(Report::msg(format!(
+                "Unexpected character {first} in token {input}"
+            ))),
         }
     }
 }
 
 pub fn try_tokenize(code: String) -> Result<Vec<Token>> {
     let mut res = vec![];
-    let mut idx = 0;
+    let mut chars = code.chars().enumerate().peekable();
 
-    while let Some(first) = code.chars().nth(idx) {
+    while let Some((_idx, first)) = chars.next() {
         match first {
             '+' | '-' | '*' | '/' | '(' | ')' => {
-                res.push(Token::try_from_str(first.to_string().as_str())?);
-                idx += 1;
+                res.push(Token::try_from_str(first.to_string())?);
             },
 
             _ if first.is_ascii_digit() => {
-                let mut token = String::new();
+                let mut token = String::from(first);
                 let mut is_frac = false;
-                while let Some(next) = code.chars().nth(idx) {
-                    if next.is_ascii_digit() {
-                        token.push(next);
-                        idx += 1;
-                    } else if next == '.' {
-                        if is_frac {
+
+                while let Some(&(idx, next)) = chars.peek() {
+                    match next {
+                        '0'..='9' => {
+                            token.push(next);
+                        },
+
+                        '.' => {
+                            if is_frac {
+                                return Err(Report::msg(format!(
+                                    "at {}: Could not parse a numeric literal with more than one dot.",
+                                    idx + 1
+                                )));
+                            }
+                            is_frac = true;
+                            token.push('.');
+                        },
+
+                        'A'..='Z' | 'a'..='z' => {
                             return Err(Report::msg(format!(
-                                "at {}: Could not parse a numeric literal with more than one dot.",
+                                "at {}: Suffixes are not supported yet",
                                 idx + 1
                             )));
-                        }
-                        is_frac = true;
-                        token.push('.');
-                        idx += 1;
-                    } else if next.is_ascii_alphabetic() {
-                        return Err(Report::msg(format!(
-                            "at {}: Suffixes are not supported yet",
-                            idx + 1
-                        )));
-                    } else {
-                        break;
+                        },
+                        _ => break,
                     }
+                    chars.next();
                 }
                 res.push(Token::NumericLiteral(token.parse()?));
             },
 
             _ if first.is_ascii_alphabetic() || first == '_' => {
-                let mut token = String::new();
-                while let Some(next) = code.chars().nth(idx) {
+                let mut token = String::from(first);
+                while let Some(&(_idx, next)) = chars.peek() {
                     if next.is_ascii_alphanumeric() || next == '_' {
                         token.push(next);
+                        chars.next();
                     } else {
                         break;
                     }
-                    idx += 1;
                 }
-                res.push(Token::try_from_str(token.as_str())?);
+                res.push(Token::try_from_str(token)?);
             },
 
-            _ if first.is_whitespace() => {
-                idx += 1;
-            },
-
+            _ if first.is_whitespace() => {},
             _ => return Err(Report::msg(format!("Could not parse charater '{first}'"))),
         }
     }
