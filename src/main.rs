9865348dd2a44ae6882e87fe7636f5516b67d143
Mouhammed Nashat Helmi <KiN4Tec@Gmail.com>
2023-08-14 12:36:01 +0300

    Implements fn evaluate

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,29 @@ mod ast;
 use color_eyre::eyre::Result;
 use ast::ASTNode;
 
+fn evaluate(ast: ASTNode) -> f64 {
+    match ast {
+        ASTNode::Number(n) => n,
+
+        ASTNode::BinaryExpr { lhs, op, rhs } => {
+            let res_lhs: f64 = evaluate(*lhs);
+            let res_rhs: f64 = evaluate(*rhs);
+
+            use lexer::Token;
+            match op {
+                Token::OpAdd => res_lhs + res_rhs,
+                Token::OpSupstract => res_lhs - res_rhs,
+                Token::OpMultiply => res_lhs * res_rhs,
+                Token::OpDivide => res_lhs / res_rhs,
+
+                _ => unimplemented!()
+            }
+        },
+
+        _ => todo!()
+    }
+}
+
 fn on_init() -> Result<()> {
     println!("\nConVector v0.0");
     Ok(())
@@ -20,8 +43,9 @@ fn on_update(repl: &mut repl::Repl, input: String) -> Result<()> {
 
     let tokens = lexer::try_tokenize(input)?;
     let ast = ASTNode::try_from(tokens)?;
+    let result = evaluate(ast);
 
-    dbg!(ast);
+    println!("ans = {result}");
 
     Ok(())
 }
