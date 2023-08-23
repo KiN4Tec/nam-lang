0d92528a52c18baba483bd2fe4f5f33fa03c51a6
Mouhammed Nashat Helmi <KiN4Tec@Gmail.com>
2023-08-23 20:06:23 +0300

    Remove implicit multiplication
    It turns out it was unclear sometimes whether we need to
    Multiply `5(2)`
    Or do a function call `exp(5)`

--- src/ast.rs
+++ src/ast.rs
@@ -51,16 +51,8 @@ impl ASTNode {
     fn parse_multiplicative_expr(tokens: &mut Vec<Token>) -> Result<Self> {
         let mut lhs = Self::parse_parenthesised_expr(tokens)?;
 
-        while tokens[0] == Token::OpMultiply
-            || tokens[0] == Token::OpDivide
-            || tokens[0] == Token::OpenParen
-        {
-            let op: Token = if tokens[0] == Token::OpenParen {
-                Token::OpMultiply
-            } else {
-                pop_front(tokens).unwrap_or_else(|| unreachable!())
-            };
-
+        while tokens[0] == Token::OpMultiply || tokens[0] == Token::OpDivide {
+            let op = pop_front(tokens).unwrap_or_else(|| unreachable!());
             let rhs = Self::parse_parenthesised_expr(tokens)?;
 
             lhs = Self::BinaryExpr {
