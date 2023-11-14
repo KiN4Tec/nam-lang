f6e21f894ab8f40b5a977331eb3240e756dc81ee
Mouhammed Nashat Helmi <KiN4Tec@Gmail.com>
2023-11-14 22:00:59 +0300

    Cleanup ast.rs
    This commit rewrites ast.rs in order to make it easier to read

--- src/ast.rs
+++ src/ast.rs
@@ -26,17 +26,20 @@ impl ASTNode {
     }
 
     fn parse_stmt(idx: usize, tokens: &[Token]) -> Result<(usize, Self), ParsingError> {
-        match (tokens.get(idx), tokens.get(idx + 1)) {
-            (Some(Token::Identifier(var_name)), Some(Token::OpAssign)) => {
-                // x = (expession)
-                let (consumed_len, var_value) = Self::parse_expr(idx + 2, tokens)?;
-                Ok((
-                    consumed_len + 2,
-                    Self::Assignment(var_name.clone(), Box::new(var_value)),
-                ))
-            },
-            (_, _) => Self::parse_expr(idx, tokens),
+        let (primary_len, primary) = Self::parse_expr(idx, tokens)?;
+
+        // Assignment Statement (x = 5)
+        if let Self::Variable(lhs) = &primary {
+            if tokens.get(idx + primary_len) == Some(&Token::OpAssign) {
+                let (rhs_len, rhs) = Self::parse_expr(idx + primary_len + 1, tokens)?;
+                return Ok((
+                    primary_len + 1 + rhs_len,
+                    Self::Assignment(lhs.to_string(), Box::new(rhs)),
+                ));
+            }
         }
+
+        Ok((primary_len, primary))
     }
 
     fn parse_expr(idx: usize, tokens: &[Token]) -> Result<(usize, Self), ParsingError> {
@@ -98,26 +101,36 @@ impl ASTNode {
         idx: usize,
         tokens: &[Token],
     ) -> Result<(usize, Self), ParsingError> {
-        if let Some(token) = tokens.get(idx) {
-            if *token != Token::OpenParen {
-                return Self::parse_primary_expr(idx, tokens);
-            }
-        } else {
-            return Err(ParsingError::UnexpectedEndOfInput);
-        }
+        // Skip if not an OpenParen
+        match tokens.get(idx) {
+            Some(Token::OpenParen) => {},
 
-        let (mut consumed_len, result) = Self::parse_expr(idx + 1, tokens)?;
-        consumed_len += 1; // Consuming the Open Paren
+            None => return Err(ParsingError::UnexpectedEndOfInput),
+            Some(_) => return Self::parse_primary_expr(idx, tokens),
+        };
 
-        if let Some(token) = tokens.get(idx + consumed_len) {
-            if *token != Token::CloseParen {
+        // Consuming the Open Paren
+        let mut consumed_len = 1;
+
+        // Parsing the expression inside
+        let (inside_len, result) = Self::parse_expr(idx + 1, tokens)?;
+        consumed_len += inside_len;
+
+        // Expect a closing paren
+        match tokens.get(idx + consumed_len) {
+            Some(Token::CloseParen) => {},
+
+            None => return Err(ParsingError::UnexpectedEndOfInput),
+            Some(_) => {
                 return Err(ParsingError::UnexpectedToken {
                     expected: Some(format!("{:?}", Token::CloseParen)),
                     found: Some(format!("{:?}", tokens[idx + consumed_len])),
-                });
-            }
+                })
+            },
         }
-        consumed_len += 1; // Consuming the Close Paren
+
+        // Consuming the Closing Paren
+        consumed_len += 1;
 
         Ok((consumed_len, result))
     }
@@ -159,7 +172,7 @@ impl std::error::Error for ParsingError {}
 impl std::fmt::Display for ParsingError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::UnexpectedEndOfInput => write!(f, "Unexpected end of input array"),
+            Self::UnexpectedEndOfInput => write!(f, "Unexpected end of input tokens array"),
             Self::UnexpectedToken { expected, found } => {
                 let mut res = String::from("Unexpected token");
                 if let Some(expected) = expected {
